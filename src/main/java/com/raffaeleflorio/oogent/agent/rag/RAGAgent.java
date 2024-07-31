e15f560f4e5cd5a81fd17e54b3357cfd7c47e122
Raffaele Florio <raffaeleflorio@protonmail.com>
2024-07-31 19:52:23 +0200

    fix RAGAgent

--- src/main/java/com/raffaeleflorio/oogent/agent/rag/RAGAgent.java
+++ src/main/java/com/raffaeleflorio/oogent/agent/rag/RAGAgent.java
@@ -5,58 +5,42 @@ import com.raffaeleflorio.oogent.LLM;
 import com.raffaeleflorio.oogent.PlainText;
 import com.raffaeleflorio.oogent.PromptTemplate;
 import com.raffaeleflorio.oogent.Response;
-import com.raffaeleflorio.oogent.Sources;
 import com.raffaeleflorio.oogent.Text;
 import com.raffaeleflorio.oogent.agent.TextResponse;
 
 import java.util.Map;
-import java.util.function.BiFunction;
 
 public final class RAGAgent implements Agent {
 
     private final Agent contextAgent;
     private final LLM llm;
     private final PromptTemplate promptTemplate;
-    private final BiFunction<? super LLM.Completion, ? super Sources, ? extends Response> responseFn;
 
     public RAGAgent(
             final Agent contextAgent,
             final LLM llm,
             final PromptTemplate promptTemplate
     ) {
-        this(
-                contextAgent,
-                llm,
-                promptTemplate,
-                TextResponse::new
-        );
-    }
-
-    public RAGAgent(
-            final Agent contextAgent,
-            final LLM llm,
-            final PromptTemplate promptTemplate,
-            final BiFunction<? super LLM.Completion, ? super Sources, ? extends Response> responseFn
-    ) {
         this.contextAgent = contextAgent;
         this.llm = llm;
         this.promptTemplate = promptTemplate;
-        this.responseFn = responseFn;
     }
 
     @Override
     public Response response(final Text text) {
         var context = this.contextAgent.response(text);
-        return this.responseFn.apply(
-                this.llm.completion(
-                        this.promptTemplate.prompt(
-                                Map.of(
-                                        new PlainText("text"), text,
-                                        new PlainText("context"), context
-                                )
+        var completion = this.llm.completion(
+                this.promptTemplate.prompt(
+                        Map.of(
+                                new PlainText("text"), text,
+                                new PlainText("context"), context
                         )
-                ),
-                context.sources()
+                )
+        );
+        return new TextResponse(
+                completion.text(),
+                context.sources(),
+                context.tokenUsage().sum(completion.tokenUsage())
         );
     }
 }
