34107c1ffc87dbcd0048ef353ad7da281bf2d757
Raffaele Florio <raffaeleflorio@protonmail.com>
2024-09-09 11:01:23 +0200

    adds ExceptionalAgent

---
+++ src/main/java/com/raffaeleflorio/oogent/agent/exception/ExceptionalAgent.java
@@ -0,0 +1,40 @@
+package com.raffaeleflorio.oogent.agent.exception;
+
+import com.raffaeleflorio.oogent.Agent;
+import com.raffaeleflorio.oogent.Response;
+import com.raffaeleflorio.oogent.Text;
+import com.raffaeleflorio.oogent.agent.TextResponse;
+
+import java.util.function.BiFunction;
+
+public final class ExceptionalAgent implements Agent {
+
+    private final Agent origin;
+    private final BiFunction<? super Text, ? super Exception, Response> responseFn;
+
+    public ExceptionalAgent(final Agent origin, final Text response) {
+        this(origin, new TextResponse(response));
+    }
+
+    public ExceptionalAgent(final Agent origin, final Response response) {
+        this(origin, (exception, text) -> response);
+    }
+
+    public ExceptionalAgent(
+            final Agent origin,
+            final BiFunction<? super Text, ? super Exception, Response> responseFn
+    ) {
+        this.origin = origin;
+        this.responseFn = responseFn;
+    }
+
+
+    @Override
+    public Response response(final Text text) {
+        try {
+            return this.origin.response(text);
+        } catch (Exception e) {
+            return this.responseFn.apply(text, e);
+        }
+    }
+}
